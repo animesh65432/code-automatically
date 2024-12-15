d31360828945502afab817c867aa89a610f4fc4a
animesh65432 <kiranduttta234@gmail.com>
2024-12-15 13:23:24 +0530

    check the llm with the prompt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,23 @@
 import "dotenv/config"
 import { GoogleGenerativeAI } from "@google/generative-ai"
 
-const MODEL_NAME = "gemini-pro";
+const MODEL_NAME = process.env.MODEL_NAME as string
 
 async function main() {
-
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
     const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+    model.generationConfig.temperature = 0.0
 
     const chat = model.startChat();
 
-    const firstResponse = await chat.sendMessage(
-        "Hello there, how are you today?"
-    );
-    console.log("Gemini:", firstResponse?.response?.text());
+    const responseStream = await chat.sendMessage("Write the code for a todo application in react or nextjs or express?");
 
-    const secondResponse = await chat.sendMessage("Can you create Todo APplication ?");
-    console.log("Gemini:", secondResponse.response?.text());
+
+    const data = await responseStream?.response
+
+    console.log(data?.text())
 }
 
 main().catch((error) => {
     console.error("Error:", error);
-});                         
+});                  
